7399945dd760f497dc5526cf7b47d9044f2846e1
Kaoru NAKAMURA <kaorun55@gmail.com>
2017-01-16 17:17:54 +0900

    プロジェクトを追加

---
+++ HoloProject/Assets/AirTapGesture.cs
@@ -0,0 +1,22 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class AirTapGesture : MonoBehaviour, IInputClickHandler
+{
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void OnInputClicked(InputEventData eventData)
+    {
+        gameObject.AddComponent<Rigidbody>();
+    }
+}
