1c71731f3d9284460592f80d9fa376dfa0d0aba9
Kaoru NAKAMURA <kaorun55@gmail.com>
2017-09-12 01:36:21 +0900

    Unity5.6.2用に差し替え

---
+++ HelloHoloProject/Assets/AirTapGesture.cs
@@ -0,0 +1,24 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class AirTapGesture : MonoBehaviour, IInputClickHandler
+{
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        gameObject.AddComponent<Rigidbody>();
+    }
+}
